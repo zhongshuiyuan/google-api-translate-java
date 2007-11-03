10196ed28a65c132ff89d6f65cf017ab297ae8be
rich.midwinter <rich.midwinter@f6142c6b-d83c-0410-bbd8-0d03b215d57c>
2007-11-03 00:18:13 +0000

    Changed user agent. Allows Arabic to work.

--- src/com/google/api/translate/Translate.java
+++ src/com/google/api/translate/Translate.java
@@ -1,12 +1,13 @@
 /**
  * Translate.java
- * 
+ *
  * Makes the Google Translate API available to Java applications.
  */
 package com.google.api.translate;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -19,32 +20,43 @@ import java.net.URLEncoder;
  */
 public class Translate {
 
-	private static final String URL_STRING = "http://translate.google.com/translate_t?langpair=";
-	private static final String TEXT_VAR = "&text=";
+    private static final String URL_STRING = "http://translate.google.com/translate_t?langpair=";
+    private static final String TEXT_VAR = "&text=";
 
-	/**
-	 * @param text The String to translate.
-	 * @param from The language code to translate from.
-	 * @param to The language code to translate to.
-	 * @return The translated String.
-	 * @throws MalformedURLException
-	 * @throws IOException
-	 */
-	public static String translate(String text, String from, String to) throws MalformedURLException, IOException {
-		StringBuffer url = new StringBuffer();
-		url.append(URL_STRING).append(from).append('|').append(to);
-		url.append(TEXT_VAR).append(URLEncoder.encode(text, "UTF-8"));
+    /**
+     * @param text The String to translate.
+     * @param from The language code to translate from.
+     * @param to The language code to translate to.
+     * @return The translated String.
+     * @throws MalformedURLException
+     * @throws IOException
+     */
+    public static String translate(String text, String from, String to) throws MalformedURLException, IOException {
+        StringBuilder url = new StringBuilder();
+        url.append(URL_STRING).append(from).append('|').append(to);
+        url.append(TEXT_VAR).append(URLEncoder.encode(text, "UTF-8"));
 
-		StringBuffer output = new StringBuffer();
-		String line;
+        StringBuilder output = new StringBuilder();
+        String line;
 
-		HttpURLConnection uc = (HttpURLConnection) new URL(url.toString()).openConnection();
-		uc.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 4.01; Windows NT)");
-		BufferedReader rd = new BufferedReader(new InputStreamReader(uc.getInputStream()));
-		while ((line = rd.readLine()) != null) output.append(line);
-		rd.close();
+        HttpURLConnection uc = (HttpURLConnection) new URL(url.toString()).openConnection();
+        uc.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
 
-		String start = output.substring(output.indexOf("<div id=result_box dir="));
-		return start.substring(27, start.indexOf("</div>"));
-	}
+        String page = toString(uc.getInputStream());
+
+        String start = page.substring(page.indexOf("<div id=result_box dir="));
+        return start.substring(27, start.indexOf("</div>"));
+    }
+
+    private static String toString(InputStream inputStream) throws IOException {
+        String string;
+        StringBuilder outputBuilder = new StringBuilder();
+        if (inputStream != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            while (null != (string = reader.readLine())) {
+                outputBuilder.append(string).append('\n');
+            }
+        }
+        return outputBuilder.toString();
+    }
 }
