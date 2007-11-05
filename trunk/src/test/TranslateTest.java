421b4d144c68da568ba8227e41118ea472baaa09
rich.midwinter <rich.midwinter@f6142c6b-d83c-0410-bbd8-0d03b215d57c>
2007-11-05 21:03:10 +0000

    Adding JUnit test

---
+++ trunk/src/test/TranslateTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package test;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+import com.google.api.translate.Language;
+import com.google.api.translate.Translate;
+
+/**
+ * @author rich
+ *
+ */
+public class TranslateTest extends TestCase {
+	@Test
+	public void testTranslate() throws MalformedURLException, IOException {
+		System.out.println("testTranslate");
+		assertEquals("مرحبا العالم", Translate.translate("Hello world", Language.ENGLISH, Language.ARABIC));
+		assertEquals("你好世界", Translate.translate("Hello world", Language.ENGLISH, Language.CHINESE));
+		assertEquals("Bonjour le monde", Translate.translate("Hello world", Language.ENGLISH, Language.FRENCH));
+		assertEquals("Hallo Welt", Translate.translate("Hello world", Language.ENGLISH, Language.GERMAN));
+		assertEquals("Ciao a tutti", Translate.translate("Hello world", Language.ENGLISH, Language.ITALIAN));
+		assertEquals("こんにちは世界", Translate.translate("Hello world", Language.ENGLISH, Language.JAPANESE));
+		assertEquals("여러분, 안녕하세요", Translate.translate("Hello world", Language.ENGLISH, Language.KOREAN));
+		assertEquals("Olá mundo", Translate.translate("Hello world", Language.ENGLISH, Language.PORTUGESE));
+		assertEquals("Привет мир", Translate.translate("Hello world", Language.ENGLISH, Language.RUSSIAN));
+		assertEquals("Hola mundo", Translate.translate("Hello world", Language.ENGLISH, Language.SPANISH));
+	}
+}
