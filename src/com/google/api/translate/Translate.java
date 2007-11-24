790c05f72974ee0d32d67880ce7c128826f00860
rich.midwinter <rich.midwinter@f6142c6b-d83c-0410-bbd8-0d03b215d57c>
2007-11-24 22:55:10 +0000

    Issue 6 in google-api-translate-java: Wrong characters in translated text (bad character decoding)

--- src/com/google/api/translate/Translate.java
+++ src/com/google/api/translate/Translate.java
@@ -19,7 +19,8 @@ import java.net.URLEncoder;
  * Makes the Google Translate API available to Java applications.
  */
 public class Translate {
-
+	
+	private static final String ENCODING = "UTF-8";
     private static final String URL_STRING = "http://translate.google.com/translate_t?langpair=";
     private static final String TEXT_VAR = "&text=";
 
@@ -34,14 +35,18 @@ public class Translate {
     public static String translate(String text, String from, String to) throws MalformedURLException, IOException {
         StringBuilder url = new StringBuilder();
         url.append(URL_STRING).append(from).append('|').append(to);
-        url.append(TEXT_VAR).append(URLEncoder.encode(text, "UTF-8"));
+        url.append(TEXT_VAR).append(URLEncoder.encode(text, ENCODING));
 
         HttpURLConnection uc = (HttpURLConnection) new URL(url.toString()).openConnection();
         uc.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
 
         String page = toString(uc.getInputStream());
+        
+        int resultBox = page.indexOf("<div id=result_box dir=");
+        
+        if (resultBox < 0) throw new Error("No translation result returned.");
 
-        String start = page.substring(page.indexOf("<div id=result_box dir="));
+        String start = page.substring(resultBox);
         return start.substring(27, start.indexOf("</div>"));
     }
 
@@ -49,7 +54,7 @@ public class Translate {
         String string;
         StringBuilder outputBuilder = new StringBuilder();
         if (inputStream != null) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, ENCODING));
             while (null != (string = reader.readLine())) {
                 outputBuilder.append(string).append('\n');
             }
