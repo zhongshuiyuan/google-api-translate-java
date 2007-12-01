ffe2a181e9189da1a3d9f1aed516308f625dfc4f
rich.midwinter <rich.midwinter@f6142c6b-d83c-0410-bbd8-0d03b215d57c>
2007-12-01 12:10:04 +0000

    Added rate control to allow throttling translation queries to prevent being blocked.

--- src/com/google/api/translate/Translate.java
+++ src/com/google/api/translate/Translate.java
@@ -23,8 +23,49 @@ public class Translate {
 	private static final String ENCODING = "UTF-8";
     private static final String URL_STRING = "http://translate.google.com/translate_t?langpair=";
     private static final String TEXT_VAR = "&text=";
+    private static final int RATE_DELAY = 2000;
+    
+    private static boolean rateControl = true;
+    private static long lastQueryTime = 0l;
+    
+    /**
+     * Are we throttling queries to prevent being blocked? Defaults to true.
+     * @return Returns true if throttling query frequency.
+     */
+    public static boolean isUsingRateControl() {
+    	return rateControl;
+    }
+    
+    /**
+     * Allows turning off rate control
+     * @param rateControl Turns on rate control if true, off if false.
+     */
+    public static void setUsingRateControl(boolean rateControl) {
+    	Translate.rateControl = rateControl;
+    }
+
+    private static String toString(InputStream inputStream) throws IOException {
+    	while (rateControl && (lastQueryTime+RATE_DELAY > System.currentTimeMillis())) {
+    		try {
+				Thread.sleep(lastQueryTime+RATE_DELAY-System.currentTimeMillis());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+    	}
+        String string;
+        StringBuilder outputBuilder = new StringBuilder();
+        if (inputStream != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, ENCODING));
+            while (null != (string = reader.readLine())) {
+                outputBuilder.append(string).append('\n');
+            }
+        }
+        if (rateControl) lastQueryTime = System.currentTimeMillis();
+        return outputBuilder.toString();
+    }
 
     /**
+     * Translates text from a given language to another given language using Google Translate
      * @param text The String to translate.
      * @param from The language code to translate from.
      * @param to The language code to translate to.
@@ -48,17 +89,5 @@ public class Translate {
 
         String start = page.substring(resultBox);
         return start.substring(27, start.indexOf("</div>"));
-    }
-
-    private static String toString(InputStream inputStream) throws IOException {
-        String string;
-        StringBuilder outputBuilder = new StringBuilder();
-        if (inputStream != null) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, ENCODING));
-            while (null != (string = reader.readLine())) {
-                outputBuilder.append(string).append('\n');
-            }
-        }
-        return outputBuilder.toString();
     }
 }
