5c6e31f2556a52a14bd1e09162d1171352a5e39c
rich.midwinter <rich.midwinter@f6142c6b-d83c-0410-bbd8-0d03b215d57c>
2008-04-24 19:09:28 +0000

    Issue 23: languages missing in "Language.validLanguages"

--- trunk/src/test/TranslateTest.java
+++ trunk/src/test/TranslateTest.java
@@ -19,13 +19,13 @@ public class TranslateTest extends TestCase {
 	public void testTranslate() throws Exception {
 		System.out.println("testTranslate");
 		assertEquals("مرحبا العالم", Translate.translate("Hello world", Language.ENGLISH, Language.ARABIC));
-		assertEquals("你好世界", Translate.translate("Hello world", Language.ENGLISH, Language.CHINESE));
-		assertEquals("Bonjour le monde", Translate.translate("Hello world", Language.ENGLISH, Language.FRENCH));
+		assertEquals("世界您好", Translate.translate("Hello world", Language.ENGLISH, Language.CHINESE));
+		assertEquals("Bonjour monde", Translate.translate("Hello world", Language.ENGLISH, Language.FRENCH));
 		assertEquals("Hallo welt", Translate.translate("Hello world", Language.ENGLISH, Language.GERMAN));
 		assertEquals("Ciao mondo", Translate.translate("Hello world", Language.ENGLISH, Language.ITALIAN));
-		assertEquals("ハローワールド", Translate.translate("Hello world", Language.ENGLISH, Language.JAPANESE));
+		assertEquals("ハロー世界", Translate.translate("Hello world", Language.ENGLISH, Language.JAPANESE));
 		assertEquals("여러분, 안녕하세요", Translate.translate("Hello world", Language.ENGLISH, Language.KOREAN));
-		assertEquals("Olá Mundo", Translate.translate("Hello world", Language.ENGLISH, Language.PORTUGESE));
+		assertEquals("Olá mundo", Translate.translate("Hello world", Language.ENGLISH, Language.PORTUGESE));
 		assertEquals("Привет мир", Translate.translate("Hello world", Language.ENGLISH, Language.RUSSIAN));
 		assertEquals("Hola mundo", Translate.translate("Hello world", Language.ENGLISH, Language.SPANISH));
 	}
@@ -34,6 +34,6 @@ public class TranslateTest extends TestCase {
 	public void testIntermediteTranslate() throws Exception {
 		System.out.println("testIntermediateTranslate");
 		assertEquals("Hola Mundo", Translate.translate("Привет мир", Language.RUSSIAN, Language.SPANISH));
-		assertEquals("Ciao mondo", Translate.translate("Hallo welt", Language.GERMAN, Language.ITALIAN));
+		assertEquals("Ciao a tutti!", Translate.translate("Hallo welt", Language.GERMAN, Language.ITALIAN));
 	}
 }
