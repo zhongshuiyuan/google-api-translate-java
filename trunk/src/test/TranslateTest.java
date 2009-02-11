d7edb60fa947a88866e228337d3b5d4a825a4e6c
rich.midwinter <rich.midwinter@f6142c6b-d83c-0410-bbd8-0d03b215d57c>
2009-02-11 20:38:45 +0000

    Added HTMLEntities to unescape HTML entities. Added new languages.

--- trunk/src/test/TranslateTest.java
+++ trunk/src/test/TranslateTest.java
@@ -22,16 +22,21 @@ public class TranslateTest extends TestCase {
 		assertEquals("مرحبا العالم", Translate.translate("Hello world", Language.ENGLISH, Language.ARABIC));
 		assertEquals("世界您好", Translate.translate("Hello world", Language.ENGLISH, Language.CHINESE));
 		assertEquals("Bonjour le monde", Translate.translate("Hello world", Language.ENGLISH, Language.FRENCH));
-		assertEquals("Hallo Welt", Translate.translate("Hello world", Language.ENGLISH, Language.GERMAN));
+		assertEquals("Hallo welt", Translate.translate("Hello world", Language.ENGLISH, Language.GERMAN));
 		assertEquals("नमस्कार दुनिया", Translate.translate("Hello world", Language.ENGLISH, Language.HINDI));
 		assertEquals("Ciao mondo", Translate.translate("Hello world", Language.ENGLISH, Language.ITALIAN));
-		assertEquals("こんにちは世界", Translate.translate("Hello world", Language.ENGLISH, Language.JAPANESE));
+		assertEquals("ハローワールド", Translate.translate("Hello world", Language.ENGLISH, Language.JAPANESE));
 		assertEquals("여보세요 세계", Translate.translate("Hello world", Language.ENGLISH, Language.KOREAN));
 		assertEquals("Olá mundo", Translate.translate("Hello world", Language.ENGLISH, Language.PORTUGESE));
 		assertEquals("Привет мир", Translate.translate("Hello world", Language.ENGLISH, Language.RUSSIAN));
 		assertEquals("Hola mundo", Translate.translate("Hello world", Language.ENGLISH, Language.SPANISH));
+		assertEquals("Hello dinja", Translate.translate("Hello world", Language.ENGLISH, Language.MALTESE));
+		assertEquals("สวัสดีโลก", Translate.translate("Hello world", Language.ENGLISH, Language.THAI));
+		assertEquals("Merhaba Dünya", Translate.translate("Hello world", Language.ENGLISH, Language.TURKISH));
 		
 		assertEquals("Hola Mundo", Translate.translate("Привет мир", Language.RUSSIAN, Language.SPANISH));
 		assertEquals("Ciao mondo", Translate.translate("Hallo welt", Language.GERMAN, Language.ITALIAN));
+		
+		assertEquals("D'accord", Translate.translate("Ok", Language.ENGLISH, Language.FRENCH));
 	}
 }
