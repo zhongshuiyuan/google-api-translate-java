edb890a9a6bb57ffd40eeeb039e368a6125529a2
rich.midwinter <rich.midwinter@f6142c6b-d83c-0410-bbd8-0d03b215d57c>
2009-09-11 21:47:48 +0000

    #63: There are not enough constants in Language enum.

--- src/test/TranslateTest.java
+++ src/test/TranslateTest.java
@@ -52,21 +52,22 @@ public class TranslateTest extends TestCase {
 		
 		assertEquals("مرحبا العالم", Translate.execute("Hello world", Language.ENGLISH, Language.ARABIC));
 		assertEquals("世界您好", Translate.execute("Hello world", Language.ENGLISH, Language.CHINESE));
-		assertEquals("Bonjour le monde", Translate.execute("Hello world", Language.ENGLISH, Language.FRENCH));
+		assertEquals("Bonjour tout le monde", Translate.execute("Hello world", Language.ENGLISH, Language.FRENCH));
 		assertEquals("Hallo Welt", Translate.execute("Hello world", Language.ENGLISH, Language.GERMAN));
-		assertEquals("नमस्कार दुनिया", Translate.execute("Hello world", Language.ENGLISH, Language.HINDI));
-		assertEquals("Ciao mondo", Translate.execute("Hello world", Language.ENGLISH, Language.ITALIAN));
-		assertEquals("こんにちは世界", Translate.execute("Hello world", Language.ENGLISH, Language.JAPANESE));
-		assertEquals("여보세요 세계", Translate.execute("Hello world", Language.ENGLISH, Language.KOREAN));
+		assertEquals("दुनिया नमस्ते", Translate.execute("Hello world", Language.ENGLISH, Language.HINDI));
+		assertEquals("Ciao a tutti", Translate.execute("Hello world", Language.ENGLISH, Language.ITALIAN));
+		assertEquals("こんにちは、世界", Translate.execute("Hello world", Language.ENGLISH, Language.JAPANESE));
+		assertEquals("안녕하세요 세상", Translate.execute("Hello world", Language.ENGLISH, Language.KOREAN));
 		assertEquals("Olá mundo", Translate.execute("Hello world", Language.ENGLISH, Language.PORTUGUESE));
 		assertEquals("Привет мир", Translate.execute("Hello world", Language.ENGLISH, Language.RUSSIAN));
-		assertEquals("Hola Mundo", Translate.execute("Hello world", Language.ENGLISH, Language.SPANISH));
+		assertEquals("¡Hola, mundo", Translate.execute("Hello world", Language.ENGLISH, Language.SPANISH));
 		assertEquals("Hello dinja", Translate.execute("Hello world", Language.ENGLISH, Language.MALTESE));
 		assertEquals("สวัสดีโลก", Translate.execute("Hello world", Language.ENGLISH, Language.THAI));
-		assertEquals("Merhaba Dünya", Translate.execute("Hello world", Language.ENGLISH, Language.TURKISH));
-		assertEquals("Hola Mundo", Translate.execute("Привет мир", Language.RUSSIAN, Language.SPANISH));
-		assertEquals("Ciao mondo", Translate.execute("Hallo welt", Language.GERMAN, Language.ITALIAN));
+		assertEquals("Merhaba dünya", Translate.execute("Hello world", Language.ENGLISH, Language.TURKISH));
+		assertEquals("¡Hola, mundo", Translate.execute("Привет мир", Language.RUSSIAN, Language.SPANISH));
+		assertEquals("Ciao a tutti", Translate.execute("Hallo welt", Language.GERMAN, Language.ITALIAN));
 		assertEquals("D'accord", Translate.execute("Ok", Language.ENGLISH, Language.FRENCH));
+		assertEquals("Iawn", Translate.execute("Ok", Language.ENGLISH, Language.WELSH));
 		
 		assertEquals("Bonjour tout le monde", Translate.execute("Привет мир", Language.AUTO_DETECT, Language.FRENCH));
 	}
@@ -77,6 +78,53 @@ public class TranslateTest extends TestCase {
 		
 		GoogleAPI.setHttpReferrer("http://code.google.com/p/google-api-translate-java/");
 		
-		assertEquals("Hello world", Translate.execute("Bonjour le monde", Language.FRENCH, Language.ENGLISH));
+		assertEquals("Hello World", Translate.execute("Bonjour le monde", Language.FRENCH, Language.ENGLISH));
+	}
+	
+	@Test
+	public void testLarge() throws Exception {
+		System.out.println("testLarge");
+		
+		Translate.execute("Figures from the Office for National Statistics (ONS) show that between December and April, "
+				+ "the five-month period typically regarded as peak bonus season, those working in the financial "
+				+ "intermediation sector received bonuses worth £7.6bn. The figure is more than 40pc lower than last"
+				+ "year's total of £13.2bn, but the fact that it came during a period where the banking system owed its"
+				+ "survival to the rescue support of taxpayers\' money will spark further outrage. Related Articles USS"
+				+ "pays bonuses despite fund fall Ex-HBOS chief Hornby gives up £1m redundancyBankers blind to bonus "
+				+ "'furore' Barclays and Lloyds to dish out millions in bonuses. City bonuses defy credit crunch and "
+				+ "hit new record of £13bn. We are still mad with the banks but we are no closer to getting even. News"
+				+ "of the huge sums being offered by Barclays to five traders at JP Morgan will also stoke the row. "
+				+ "Barclays is close to poaching Todd Edgar, 37, a star commodity trader at JP Morgan, and his four "
+				+ "team members to head up the foreign exchange trading desk. Mr Edgar is responsible for a $2bn book "
+				+ "at JP Morgan and single-handedly made the US investment bank a $100m profit last year. At Barclays,"
+				+ "the team will have an emerging markets focus, with two members based in Asia and Mr Edgar and the "
+				+ "others operating out of London. They will also continue to trade commodities, but to a lesser degree"
+				+ "than before. Barclays has offered the team a combined $25m in salaries and bonuses paid in cash "
+				+ "guarantees and deferred stock. In addition, they will take a share of future profits that could lift"
+				+ "the package to $50m. Market-leading rates on profit shares are currently 12pc, according to bankers,"
+				+ "but Mr Edgar and his team are said to have been offered even more generous terms. Sources suggest Mr"
+				+ "Edgar himself could receive up to half the total. It is understood the pay package does not "
+				+ "contravene any of the Financial Service Authority's guidelines. At JP Morgan, Mr Edgar was largely a"
+				+ "proprietary trader, gambling with the bank's own money. At Barclays, although he will take "
+				+ "proprietary positions, his main role will be client business. Mr Edgar's appointment would follow "
+				+ "public outrage last week over a £7m \"market leading\" deal agreed by Royal Bank of Scotland, 70pc "
+				+ "owned by the taxpayer, for a Merrill Lynch banker, Antonio Polverino. Although Barclays has not "
+				+ "taken any cash directly from the state, critics say it is the beneficiary of £1.2 trillion of "
+				+ "taxpayer support for the financial system as a whole. Senior Treasury officials believe that the "
+				+ "bank would have collapsed were it not for their assistance. In an interview this weekend, the Shadow"
+				+ "Chancellor, George Osborne said it was \"totally unacceptable\" that the major banks are paying "
+				+ "large bonuses on the back of taxpayer guarantees. Mr Osborne said: \"There are hundreds of billions "
+				+ "of pounds of guarantees in existence: guarantees provided by the taxpayer to all banks. The reason "
+				+ "those guarantees are in place is not so the bankers can pay themselves large bonuses. \"The scale of"
+				+ "this year's bonus payments, as revealed by the ONS statistics, would be enough to finance an almost "
+				+ "2p reduction in the basic rate of income tax. The payments came after the unprecedented bail-out of "
+				+ "British banks, which cost the taxpayer some £35bn in capital infusions. Lord Oakeshott, Liberal "
+				+ "Democrat Treasury spokesman, said: \"These figures suggest that the bankers are taking most of the "
+				+ "profits and the taxpayer is taking most of the risk. \"The official confirmation of the scale of "
+				+ "City bonuses in the past year underlines the fact that even against the backdrop of the worst "
+				+ "financial crisis in British history, bankers awarded themselves bonuses which were still "
+				+ "significantly larger, even in nominal terms, than those handed out five years ago in 2004, when the "
+				+ "City was entering the credit boom. Barclays and JP Morgan declined to comment.",
+				Language.ENGLISH, Language.FRENCH);
 	}
 }
